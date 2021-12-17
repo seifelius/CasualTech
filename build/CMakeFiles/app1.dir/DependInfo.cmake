5171215f3ce944f6d0d4b1ba5a8f02810be7bab8
Daniil Dudenkov <62514215+seifelius@users.noreply.github.com>
2021-12-17 21:53:18 +0200

    Add files via upload

---
+++ build/CMakeFiles/app1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Startup_C/build/app1.cpp" "/home/pi/Startup_C/build/CMakeFiles/app1.dir/app1.cpp.o"
+  "/home/pi/Startup_C/src/Message.cpp" "/home/pi/Startup_C/build/CMakeFiles/app1.dir/src/Message.cpp.o"
+  "/home/pi/Startup_C/src/ResponseHandler.cpp" "/home/pi/Startup_C/build/CMakeFiles/app1.dir/src/ResponseHandler.cpp.o"
+  "/home/pi/Startup_C/src/SerialComManager.cpp" "/home/pi/Startup_C/build/CMakeFiles/app1.dir/src/SerialComManager.cpp.o"
+  "/home/pi/Startup_C/src/example1.cpp" "/home/pi/Startup_C/build/CMakeFiles/app1.dir/src/example1.cpp.o"
+  "/home/pi/Startup_C/src/serialPortHandler.cpp" "/home/pi/Startup_C/build/CMakeFiles/app1.dir/src/serialPortHandler.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/app1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app1.dir/app1.cpp.o"
+  "CMakeFiles/app1.dir/src/Message.cpp.o"
+  "CMakeFiles/app1.dir/src/ResponseHandler.cpp.o"
+  "CMakeFiles/app1.dir/src/SerialComManager.cpp.o"
+  "CMakeFiles/app1.dir/src/example1.cpp.o"
+  "CMakeFiles/app1.dir/src/serialPortHandler.cpp.o"
+  "app1"
+  "app1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
